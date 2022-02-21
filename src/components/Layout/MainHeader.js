02b034e0e55bf4bec80755aa6d84a534edc03925
zeratul117 <luisbelis@hotmail.com>
2022-02-21 18:55:51 -0500

    logo and name change

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -3,7 +3,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
     return (
         <header className={classes.header}>
-            <img src="/logo.png" alt="image" />
+            <img src="https://cdn.iconscout.com/icon/free/png-256/currency-conversion-1824292-1545914.png" alt="image" />
             <h1>Currency Converter</h1>
         </header>
     );
